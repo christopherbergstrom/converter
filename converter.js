c92edda8358dc38bf426c666778ebda7444ee39a
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-06-06 15:07:54 -0600

    populated DTR

--- converter.js
+++ converter.js
@@ -136,7 +136,88 @@ function popArea()
 }
 function popDTR()
 {
-  console.log("popDTR");
+  removeOptions();
+  for (var i = 0; i < 2; i++)
+  {
+    var option = document.createElement("option");
+    option.innerHTML="Bit per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Kilo per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Kilobyte per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Kibibit per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Megabit per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Megabyte per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Mebibit per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Gigabit per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Gigabyte per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Gigibit per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Terabit per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Terabyte per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+    var option = document.createElement("option");
+    option.innerHTML="Teribit per second";
+    if (i === 0)
+      leftSelect.add(option);
+    if (i === 1)
+      rightSelect.add(option);
+  }
 }
 function popDigitalStorage()
 {
