99659227104c96217f7708a4a1cba71a736948d3
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-06-08 16:57:20 -0600

    area skeleton done

--- js/Area.js
+++ js/Area.js
@@ -1,77 +1,501 @@
 var Area = function(){};
 Area.prototype.left = function(one, two, num)
 {
-  if (one === "Square kilometer" && two === "Square meter")
-    rightIn.value = area.k2Tom2(num);
-  else if (one === "Square meter" && two === "Square kilometer")
-    rightIn.value = area.m2Tok2(num);
-  else if (one === "Celcius" && two === "Kelvin")
-    rightIn.value = area.cToK(num);
-  else if (one === "Kelvin" && two === "Celcius")
-    rightIn.value = area.kToC(num);
-  else if (one === "Fahrenheit" && two === "Kelvin")
-    rightIn.value = area.fToK(num);
-  else if (one === "Kelvin" && two === "Fahrenheit")
-    rightIn.value = area.kToF(num);
-  else if (one === "Square kilometer" && two === "Square kilometer")
+  if (one === "Square kilometer" && two === "Square kilometer")
     rightIn.value = num;
+  else if (one === "Square kilometer" && two === "Square meter")
+    rightIn.value = area.kmTomMeter(num);
+  else if (one === "Square kilometer" && two === "Square mile")
+    rightIn.value = area.kmToMile(num);
+  else if (one === "Square kilometer" && two === "Square yard")
+    rightIn.value = area.kmToYard(num);
+  else if (one === "Square kilometer" && two === "Square foot")
+    rightIn.value = area.kmToFoot(num);
+  else if (one === "Square kilometer" && two === "Square inch")
+    rightIn.value = area.kmToInch(num);
+  else if (one === "Square kilometer" && two === "Hectare")
+    rightIn.value = area.kmToHectare(num);
+  else if (one === "Square kilometer" && two === "Acre")
+    rightIn.value = area.kmToAcre(num);
+
+  else if (one === "Square meter" && two === "Square kilometer")
+    rightIn.value = area.meterToKm(num);
   else if (one === "Square meter" && two === "Square meter")
     rightIn.value = num;
+  else if (one === "Square meter" && two === "Square mile")
+    rightIn.value = area.meterToMile(num);
+  else if (one === "Square meter" && two === "Square yard")
+    rightIn.value = area.meterToYard(num);
+  else if (one === "Square meter" && two === "Square foot")
+    rightIn.value = area.meterToFoot(num);
+  else if (one === "Square meter" && two === "Square inch")
+    rightIn.value = area.meterToInch(num);
+  else if (one === "Square meter" && two === "Hectare")
+    rightIn.value = area.meterToHectare(num);
+  else if (one === "Square meter" && two === "Acre")
+    rightIn.value = area.meterToAcre(num);
+
+  else if (one === "Square mile" && two === "Square kilometer")
+    rightIn.value = area.mileToKm(num);
+  else if (one === "Square mile" && two === "Square meter")
+    rightIn.value = area.mileToMeter(num);
   else if (one === "Square mile" && two === "Square mile")
     rightIn.value = num;
+  else if (one === "Square mile" && two === "Square yard")
+    rightIn.value = area.mileToYard(num);
+  else if (one === "Square mile" && two === "Square foot")
+    rightIn.value = area.mileToFoot(num);
+  else if (one === "Square mile" && two === "Square inch")
+    rightIn.value = area.mileToInch(num);
+  else if (one === "Square mile" && two === "Hectare")
+    rightIn.value = area.mileToHectare(num);
+  else if (one === "Square mile" && two === "Acre")
+    rightIn.value = area.mileToAcre(num);
+
+  else if (one === "Square yard" && two === "Square kilometer")
+    rightIn.value = area.yardToKm(num);
+  else if (one === "Square yard" && two === "Square meter")
+    rightIn.value = area.yardToMeter(num);
+  else if (one === "Square yard" && two === "Square mile")
+    rightIn.value = area.yardToMile(num);
   else if (one === "Square yard" && two === "Square yard")
     rightIn.value = num;
+  else if (one === "Square yard" && two === "Square foot")
+    rightIn.value = area.yardToFoot(num);
+  else if (one === "Square yard" && two === "Square inch")
+    rightIn.value = area.yardToInch(num);
+  else if (one === "Square yard" && two === "Hectare")
+    rightIn.value = area.yardToHectare(num);
+  else if (one === "Square yard" && two === "Acre")
+    rightIn.value = area.yardToAcre(num);
+
+  else if (one === "Square foot" && two === "Square kilometer")
+    rightIn.value = area.footToKm(num);
+  else if (one === "Square foot" && two === "Square meter")
+    rightIn.value = area.footToMeter(num);
+  else if (one === "Square foot" && two === "Square mile")
+    rightIn.value = area.footToMile(num);
+  else if (one === "Square foot" && two === "Square yard")
+    rightIn.value = area.footToYard(num);
   else if (one === "Square foot" && two === "Square foot")
     rightIn.value = num;
+  else if (one === "Square foot" && two === "Square inch")
+    rightIn.value = area.footToInch(num);
+  else if (one === "Square foot" && two === "Hectare")
+    rightIn.value = area.footToHectare(num);
+  else if (one === "Square foot" && two === "Acre")
+    rightIn.value = area.footToAcre(num);
+
+  else if (one === "Square inch" && two === "Square kilometer")
+    rightIn.value = area.inchToKm(num);
+  else if (one === "Square inch" && two === "Square meter")
+    rightIn.value = area.inchToMeter(num);
+  else if (one === "Square inch" && two === "Square mile")
+    rightIn.value = area.inchToMile(num);
+  else if (one === "Square inch" && two === "Square yard")
+    rightIn.value = area.inchToYard(num);
+  else if (one === "Square inch" && two === "Square foot")
+    rightIn.value = area.inchToFoot(num);
   else if (one === "Square inch" && two === "Square inch")
     rightIn.value = num;
+  else if (one === "Square inch" && two === "Hectare")
+    rightIn.value = area.inchToHectare(num);
+  else if (one === "Square inch" && two === "Acre")
+    rightIn.value = area.inchToAcre(num);
+
+  else if (one === "Hectare" && two === "Square kilometer")
+    rightIn.value = area.hectareToKm(num);
+  else if (one === "Hectare" && two === "Square meter")
+    rightIn.value = area.hectareToMeter(num);
+  else if (one === "Hectare" && two === "Square mile")
+    rightIn.value = area.hectareToMile(num);
+  else if (one === "Hectare" && two === "Square yard")
+    rightIn.value = area.hectareToYard(num);
+  else if (one === "Hectare" && two === "Square foot")
+    rightIn.value = area.hectareToFoot(num);
+  else if (one === "Hectare" && two === "Square inch")
+    rightIn.value = area.hectareToInch(num);
   else if (one === "Hectare" && two === "Hectare")
     rightIn.value = num;
+  else if (one === "Hectare" && two === "Acre")
+    rightIn.value = area.hectareToAcre(num);
+
+  else if (one === "Acre" && two === "Square kilometer")
+    rightIn.value = area.acreToKm(num);
+  else if (one === "Acre" && two === "Square meter")
+    rightIn.value = area.acreToMeter(num);
+  else if (one === "Acre" && two === "Square mile")
+    rightIn.value = area.acreToMile(num);
+  else if (one === "Acre" && two === "Square yard")
+    rightIn.value = area.acreToYard(num);
+  else if (one === "Acre" && two === "Square foot")
+    rightIn.value = area.acreToFoot(num);
+  else if (one === "Acre" && two === "Square inch")
+    rightIn.value = area.acreToInch(num);
+  else if (one === "Acre" && two === "Hectare")
+    rightIn.value = area.acreToHectare(num);
   else if (one === "Acre" && two === "Acre")
     rightIn.value = num;
 }
 Area.prototype.right = function(one, two, num)
 {
-  if (one === "Celcius" && two === "Fahrenheit")
-    leftIn.value = area.fToC(num);
-  else if (one === "Fahrenheit" && two === "Celcius")
-    leftIn.value = area.cToF(num);
-  else if (one === "Celcius" && two === "Kelvin")
-    leftIn.value = area.kToC(num);
-  else if (one === "Kelvin" && two === "Celcius")
-    leftIn.value = area.cToK(num);
-  else if (one === "Fahrenheit" && two === "Kelvin")
-    leftIn.value = area.kToF(num);
-  else if (one === "Kelvin" && two === "Fahrenheit")
-    leftIn.value = area.fToK(num);
-  else if (one === "Fahrenheit" && two === "Fahrenheit")
-    rightIn.value = num;
-  else if (one === "Celcius" && two === "Celcius")
-    rightIn.value = num;
-  else if (one === "Kelvin" && two === "Kelvin")
-    rightIn.value = num;
+  if (one === "Square kilometer" && two === "Square kilometer")
+    leftIn.value = num;
+  else if (one === "Square kilometer" && two === "Square meter")
+    leftIn.value = area.meterToKm(num);
+  else if (one === "Square kilometer" && two === "Square mile")
+    leftIn.value = area.mileToKm(num);
+  else if (one === "Square kilometer" && two === "Square yard")
+    leftIn.value = area.yardToKm(num);
+  else if (one === "Square kilometer" && two === "Square foot")
+    leftIn.value = area.footToKm(num);
+  else if (one === "Square kilometer" && two === "Square inch")
+    leftIn.value = area.inchToKm(num);
+  else if (one === "Square kilometer" && two === "Hectare")
+    leftIn.value = area.hectareToKm(num);
+  else if (one === "Square kilometer" && two === "Acre")
+    leftIn.value = area.acreToKm(num);
+
+  else if (one === "Square meter" && two === "Square kilometer")
+    leftIn.value = area.kmTomMeter(num);
+  else if (one === "Square meter" && two === "Square meter")
+    leftIn.value = num;
+  else if (one === "Square meter" && two === "Square mile")
+    leftIn.value = area.mileToMeter(num);
+  else if (one === "Square meter" && two === "Square yard")
+    leftIn.value = area.yardToMeter(num);
+  else if (one === "Square meter" && two === "Square foot")
+    leftIn.value = area.footToMeter(num);
+  else if (one === "Square meter" && two === "Square inch")
+    leftIn.value = area.inchToMeter(num);
+  else if (one === "Square meter" && two === "Hectare")
+    leftIn.value = area.hectareToMeter(num);
+  else if (one === "Square meter" && two === "Acre")
+    leftIn.value = area.acreToMeter(num);
+
+  else if (one === "Square mile" && two === "Square kilometer")
+    leftIn.value = area.kmToMile(num);
+  else if (one === "Square mile" && two === "Square meter")
+    leftIn.value = area.meterToMile(num);
+  else if (one === "Square mile" && two === "Square mile")
+    leftIn.value = num;
+  else if (one === "Square mile" && two === "Square yard")
+    leftIn.value = area.yardToMile(num);
+  else if (one === "Square mile" && two === "Square foot")
+    leftIn.value = area.footToMile(num);
+  else if (one === "Square mile" && two === "Square inch")
+    leftIn.value = area.inchToMile(num);
+  else if (one === "Square mile" && two === "Hectare")
+    leftIn.value = area.hectareToMile(num);
+  else if (one === "Square mile" && two === "Acre")
+    leftIn.value = area.acreToMile(num);
+
+  else if (one === "Square yard" && two === "Square kilometer")
+    leftIn.value = area.kmToYard(num);
+  else if (one === "Square yard" && two === "Square meter")
+    leftIn.value = area.meterToYard(num);
+  else if (one === "Square yard" && two === "Square mile")
+    leftIn.value = area.mileToYard(num);
+  else if (one === "Square yard" && two === "Square yard")
+    leftIn.value = num;
+  else if (one === "Square yard" && two === "Square foot")
+    leftIn.value = area.footToYard(num);
+  else if (one === "Square yard" && two === "Square inch")
+    leftIn.value = area.inchToYard(num);
+  else if (one === "Square yard" && two === "Hectare")
+    leftIn.value = area.hectareToYard(num);
+  else if (one === "Square yard" && two === "Acre")
+    leftIn.value = area.acreToYard(num);
+
+  else if (one === "Square foot" && two === "Square kilometer")
+    leftIn.value = area.kmToFoot(num);
+  else if (one === "Square foot" && two === "Square meter")
+    leftIn.value = area.meterToFoot(num);
+  else if (one === "Square foot" && two === "Square mile")
+    leftIn.value = area.mileToFoot(num);
+  else if (one === "Square foot" && two === "Square yard")
+    leftIn.value = area.yardToFoot(num);
+  else if (one === "Square foot" && two === "Square foot")
+    leftIn.value = num;
+  else if (one === "Square foot" && two === "Square inch")
+    leftIn.value = area.inchToFoot(num);
+  else if (one === "Square foot" && two === "Hectare")
+    leftIn.value = area.hectareToFoot(num);
+  else if (one === "Square foot" && two === "Acre")
+    leftIn.value = area.acreToFoot(num);
+
+  else if (one === "Square inch" && two === "Square kilometer")
+    leftIn.value = area.kmToInch(num);
+  else if (one === "Square inch" && two === "Square meter")
+    leftIn.value = area.meterToInch(num);
+  else if (one === "Square inch" && two === "Square mile")
+    leftIn.value = area.mileToInch(num);
+  else if (one === "Square inch" && two === "Square yard")
+    leftIn.value = area.yardToInch(num);
+  else if (one === "Square inch" && two === "Square foot")
+    leftIn.value = area.footToInch(num);
+  else if (one === "Square inch" && two === "Square inch")
+    leftIn.value = num;
+  else if (one === "Square inch" && two === "Hectare")
+    leftIn.value = area.hectareToInch(num);
+  else if (one === "Square inch" && two === "Acre")
+    leftIn.value = area.acreToInch(num);
+
+  else if (one === "Hectare" && two === "Square kilometer")
+    leftIn.value = area.kmToHectare(num);
+  else if (one === "Hectare" && two === "Square meter")
+    leftIn.value = area.meterToHectare(num);
+  else if (one === "Hectare" && two === "Square mile")
+    leftIn.value = area.mileToHectare(num);
+  else if (one === "Hectare" && two === "Square yard")
+    leftIn.value = area.yardToHectare(num);
+  else if (one === "Hectare" && two === "Square foot")
+    leftIn.value = area.footToHectare(num);
+  else if (one === "Hectare" && two === "Square inch")
+    leftIn.value = area.inchToHectare(num);
+  else if (one === "Hectare" && two === "Hectare")
+    leftIn.value = num;
+  else if (one === "Hectare" && two === "Acre")
+    leftIn.value = area.acreToHectare(num);
+
+  else if (one === "Acre" && two === "Square kilometer")
+    leftIn.value = area.kmToAcre(num);
+  else if (one === "Acre" && two === "Square meter")
+    leftIn.value = area.meterToAcre(num);
+  else if (one === "Acre" && two === "Square mile")
+    leftIn.value = area.mileToAcre(num);
+  else if (one === "Acre" && two === "Square yard")
+    leftIn.value = area.yardToAcre(num);
+  else if (one === "Acre" && two === "Square foot")
+    leftIn.value = area.footToAcre(num);
+  else if (one === "Acre" && two === "Square inch")
+    leftIn.value = area.inchToAcre(num);
+  else if (one === "Acre" && two === "Hectare")
+    leftIn.value = area.hectareToAcre(num);
+  else if (one === "Acre" && two === "Acre")
+    leftIn.value = num;
 }
-Area.prototype.cToF = function(num)
+Area.prototype.kmTomMeter = function(num)
 {
-  return (num * 9/5) + 32;
+  return ;
 }
-Area.prototype.fToC = function(num)
+Area.prototype.kmToMile = function(num)
 {
-  return (num - 32) * (5/9);
+  return ;
 }
-Area.prototype.cToK = function(num)
+Area.prototype.kmToYard = function(num)
 {
-  return num + 273.15;
+  return ;
 }
-Area.prototype.kToC = function(num)
+Area.prototype.kmToFoot = function(num)
 {
-  return num - 273.15;
+  return ;
 }
-Area.prototype.fToK = function(num)
+Area.prototype.kmToInch = function(num)
 {
-  return (num + 459.67) * (5/9);
+  return ;
 }
-Area.prototype.kToF = function(num)
+Area.prototype.kmToHectare = function(num)
 {
-  return (num * (9/5)) - 459.67;
+  return ;
+}
+Area.prototype.kmToAcre = function(num)
+{
+  return ;
+}
+Area.prototype.meterToKm = function(num)
+{
+  return ;
+}
+Area.prototype.meterToMile = function(num)
+{
+  return ;
+}
+Area.prototype.meterToYard = function(num)
+{
+  return ;
+}
+Area.prototype.meterToFoot = function(num)
+{
+  return ;
+}
+Area.prototype.meterToInch = function(num)
+{
+  return ;
+}
+Area.prototype.meterToHectare = function(num)
+{
+  return ;
+}
+Area.prototype.meterToAcre = function(num)
+{
+  return ;
+}
+Area.prototype.mileToKm = function(num)
+{
+  return ;
+}
+Area.prototype.mileToMeter = function(num)
+{
+  return ;
+}
+Area.prototype.mileToYard = function(num)
+{
+  return ;
+}
+Area.prototype.mileToFoot = function(num)
+{
+  return ;
+}
+Area.prototype.mileToInch = function(num)
+{
+  return ;
+}
+Area.prototype.mileToHectare = function(num)
+{
+  return ;
+}
+Area.prototype.mileToAcre = function(num)
+{
+  return ;
+}
+Area.prototype.yardToKm = function(num)
+{
+  return ;
+}
+Area.prototype.yardToMeter = function(num)
+{
+  return ;
+}
+Area.prototype.yardToMile = function(num)
+{
+  return ;
+}
+Area.prototype.yardToFoot = function(num)
+{
+  return ;
+}
+Area.prototype.yardToInch = function(num)
+{
+  return ;
+}
+Area.prototype.yardToHectare = function(num)
+{
+  return ;
+}
+Area.prototype.yardToAcre = function(num)
+{
+  return ;
+}
+Area.prototype.footToKm = function(num)
+{
+  return ;
+}
+Area.prototype.footToMeter = function(num)
+{
+  return ;
+}
+Area.prototype.footToMile = function(num)
+{
+  return ;
+}
+Area.prototype.footToYard = function(num)
+{
+  return ;
+}
+Area.prototype.footToInch = function(num)
+{
+  return ;
+}
+Area.prototype.footToHectare = function(num)
+{
+  return ;
+}
+Area.prototype.footToAcre = function(num)
+{
+  return ;
+}
+Area.prototype.inchToKm = function(num)
+{
+  return ;
+}
+Area.prototype.inchToMeter = function(num)
+{
+  return ;
+}
+Area.prototype.inchToMile = function(num)
+{
+  return ;
+}
+Area.prototype.inchToYard = function(num)
+{
+  return ;
+}
+Area.prototype.inchToFoot = function(num)
+{
+  return ;
+}
+Area.prototype.inchToHectare = function(num)
+{
+  return ;
+}
+Area.prototype.inchToAcre = function(num)
+{
+  return ;
+}
+Area.prototype.hectareToKm = function(num)
+{
+  return ;
+}
+Area.prototype.hectareToMeter = function(num)
+{
+  return ;
+}
+Area.prototype.hectareToMile = function(num)
+{
+  return ;
+}
+Area.prototype.hectareToYard = function(num)
+{
+  return ;
+}
+Area.prototype.hectareToFoot = function(num)
+{
+  return ;
+}
+Area.prototype.hectareToInch = function(num)
+{
+  return ;
+}
+Area.prototype.hectareToAcre = function(num)
+{
+  return ;
+}
+Area.prototype.acreToKm = function(num)
+{
+  return ;
+}
+Area.prototype.acreToMeter = function(num)
+{
+  return ;
+}
+Area.prototype.acreToMile = function(num)
+{
+  return ;
+}
+Area.prototype.acreToYard = function(num)
+{
+  return ;
+}
+Area.prototype.acreToFoot = function(num)
+{
+  return ;
+}
+Area.prototype.acreToInch = function(num)
+{
+  return ;
+}
+Area.prototype.acreToHectare = function(num)
+{
+  return ;
 }
